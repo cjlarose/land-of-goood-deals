fcfdc548888e82357bbb69455f8eb7800eb65713
Chris LaRose <cjlarose@gmail.com>
2014-04-16 16:50:46 -0700

    Dave's a fuckin tool

--- about.php
+++ about.php
@@ -3,8 +3,6 @@
 	$title = "About Us";
 	$description = "This is all about us";
 ?>
-    <div id="content">
-<!-- end header -->
 <h2>About Us</h2>
 <p>You love <em>Adventure Time</em>, and so do we! And you (probably) love
 buying <em>Adventure Time</em> merchandise, and we have such stuff because
@@ -14,10 +12,6 @@ Sounds good? ;)</p>
 <p><strong>Dave Fei</strong> is a fan of Pendleton Ward's famous documentary
 of the lives of Jake the Dog and Finn the Human. He wrote some of the html pages for this website because he's really a homeless guy for hire with poor programming skills, no job, and no women. In his spare time (which he probably doesn't have), he participates in screaming contests with the Earl of Lemongrab, gets eye strain from BMO's last-gen graphics, and laughs at how Marceline can't find an appropriate boyfriend/potential spouse because she's more than a thousand years old. Admit it's pretty hilarious.</p>
 <p><strong>Chris LaRose</strong> is also a fan of Pendleton Ward's work as well. But remember he's NOT a true fan of <em>Adventure Time</em> because he only watched three episodes. Anyhow, as you know, he's also a Webmaster for this website (even though he could totally do without Dave's help because Chris is a boss (but not really because Justin Giboney IS the boss)).</p>
-<!-- begin footer -->
-    </div>
-    <footer>
-        &copy; Chris LaRose &amp; Dave Fei
 <?php
 	include 'footer.php';
 ?>
