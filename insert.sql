05bdb447eff83cbfa5d3e47ee5ccb3debe75aea2
Chris LaRose <cjlarose@gmail.com>
2014-04-16 17:25:50 -0700

    Add products sql

---
+++ insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO `product` (name, description, image, price) VALUES ('Princess Bubblegum Stickers', 'Bubblegum stickers', 'princess-bubblegum.png', 12.34);
+INSERT INTO `product` (name, description, image, price) VALUES ('Dating Tips from the Ice King', 'Best book from the Ice King', 'ice-king.png', 21.99);
+INSERT INTO `product` (name, description, image, price) VALUES ('Finn\'s Hat', 'Cool hate', 'finn.png', 42.99);
+INSERT INTO `product` (name, description, image, price) VALUES ('Jake plush toy', 'Cool plush toy', 'jake.png', 99.99);
