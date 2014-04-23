fec70ad18ece393401441c50bfb2aea7d6f913bc
Chris LaRose <cjlarose@gmail.com>
2014-04-22 22:38:53 -0700

    Update totals on cart page after remove

--- js/remove_element.js
+++ js/remove_element.js
@@ -8,11 +8,26 @@ $(document).ready(function() {
         $.post("removefromcart.php", payload)
             .done(function() {
                 element.remove();
+                updateTotal();
             })
             .error(function() {
                 throw "Bad response from server";
             });
-    };
+    }
+
+    function getTotal() {
+        var total = 0;
+        $("#cart-items tbody tr[data-line-total]").each(function(i, e) {
+            var lineTotal = parseFloat($(e).data('line-total'));
+            total += lineTotal;
+        });
+        return total;
+    }
+
+    function updateTotal() {
+        var total = getTotal();
+        $("#total_price").text("$" + total);
+    }
 
     $("#cart-items .delete-item").click(function(e) {
         e.preventDefault();
